8719ed260a898c7f66f5faa755baf73e76645ff1
Christopher Hicks <chrhicks@gmail.com>
2024-02-02 09:25:51 -0500

    clean up state

--- lib/providers/UniverseStateProvider.tsx
+++ lib/providers/UniverseStateProvider.tsx
@@ -6,19 +6,6 @@ import { configuration } from "../config";
 
 export type IncrementalTypes = 'upQuark' | 'downQuark' | 'proton' | 'neutron' | 'electron'
 
-interface Incremental {
-  value: number
-  setValue: Dispatch<SetStateAction<number>>
-}
-
-export interface UniverseStateProps {
-  incrementals: Record<IncrementalTypes, Incremental>
-  energy: number
-  setEnergy: Dispatch<SetStateAction<number>>
-  experience: number
-  setExperience: Dispatch<SetStateAction<number>>
-}
-
 export interface ThingValue {
   thingType: IncrementalTypes
   total: number
@@ -47,7 +34,6 @@ export interface StateWrapper {
   setUniverseState: Dispatch<SetStateAction<UniverseState>>
 }
 
-const UniverseStateContext = createContext<UniverseStateProps>(undefined!)
 const UniverseStateCtx = createContext<StateWrapper>(undefined!)
 
 const defaultUniverseState: UniverseState = {
@@ -92,14 +78,6 @@ const defaultUniverseState: UniverseState = {
 }
 
 export function UniverseStateProvider({ children }: { children: React.ReactNode }) {
-  const [energy, setEnergy] = useState(0)
-  const [experience, setExperience] = useState(0)
-  const [uQuarks, setUQuarks] = useState(0)
-  const [dQuarks, setDQuarks] = useState(0)
-  const [protons, setProtons] = useState(0)
-  const [neutrons, setNeutrons] = useState(0)
-  const [electrons, setElectrons] = useState(0)
-
   const [universeState, setUniverseState] = useState<UniverseState>(defaultUniverseState)
 
   useEffect(() => {
@@ -116,52 +94,9 @@ export function UniverseStateProvider({ children }: { children: React.ReactNode 
       universeState,
       setUniverseState
     }}>
-      <UniverseStateContext.Provider
-        value={{
-          incrementals: {
-            'upQuark': {
-              value: uQuarks,
-              setValue: setUQuarks
-            },
-            'downQuark': {
-              value: dQuarks,
-              setValue: setDQuarks
-            },
-            'proton': {
-              value: protons,
-              setValue: setProtons
-            },
-            'neutron': {
-              value: neutrons,
-              setValue: setNeutrons
-            },
-            'electron': {
-              value: electrons,
-              setValue: setElectrons
-            }
-          },
-          energy,
-          setEnergy,
-          experience,
-          setExperience
-        }}
-      >
-        {children}
-      </UniverseStateContext.Provider>
+      {children}
     </UniverseStateCtx.Provider>
   )
-}
-
-export function useUniverseStateContext(): UniverseStateProps {
-  const context = useContext(UniverseStateContext)
-
-  if (!context) {
-    throw new Error(
-      "useUniverseStateContext should be used within the UniverseState provider!"
-    )
-  }
-
-  return context
 }
 
 export function useUniverseStateCtx(): StateWrapper {
